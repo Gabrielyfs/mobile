035957f9c670ac9a0b04f2f892de4bf403157b6e
Gabriely Félix <felixdesouzagabriely@gmail.com>
2025-03-27 14:30:43 -0300

    Delete mobile directory

--- mobile/Produto-API/prisma/migrations/20250318164619_init/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "Produto" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "name" TEXT NOT NULL,
-    "price" REAL NOT NULL,
-    "description" TEXT NOT NULL,
-    "ingredients" TEXT NOT NULL,
-    "imgUrl" TEXT NOT NULL
-);
